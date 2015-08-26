3310984af7572a019558fdee190d3f88eed3fa6d
narae427 <narae427@gmail.com>
2015-08-26 15:54:20 +0900

    PM

---
+++ TOPS_Daemon/src/tops/struct/PM.java
@@ -0,0 +1,25 @@
+package tops.struct;
+
+public class PM {
+	public String commandMessage;
+	public String idMessage;
+	public String pvMessage;
+	public String pvipMessage;
+	public String pvpnMessage;
+	public String pbMessage;
+	public String pbipMessage;
+	public String pbpnMessage;
+	public String fidMessage;
+	public String fnameMessage;
+	public String modMessage;
+	public String expMessage;
+	public String freindsInfoMessage;
+	
+	public String fi_idMessage;
+	public String fi_pvMessage;
+	public String fi_pvipMessage;
+	public String fi_pvpnMessage;
+	public String fi_pbMessage;
+	public String fi_pbipMessage;
+	public String fi_pbpnMessage;
+}
